9c41121d65fe0d3b8bf455135181c2f1ec7553e9
Zach <zcox@alightanalytics.com>
2018-07-21 12:37:27 -0500

    Changed method names.

--- smart_cart/app.py
+++ smart_cart/app.py
@@ -1,26 +1,32 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', methods = ['GET'])
 def index():
     return 'Index Page'
 
+
 @app.route('/start-cart/', methods = ['POST'])
-def projects():
+def start_cart():
     return 'The Start Cart Page'
 
+
 @app.route('/add-item/', methods = ['POST'])
-def projects():
+def add_item():
     return 'The Add Item Page'
 
+
 @app.route('/delete-item/', methods = ['DELETE'])
-def projects():
+def delete_item():
     return 'The Delete Item Page'
 
+
 @app.route('/view-cart/', methods = ['GET'])
-def projects():
+def view_cart():
     return 'The View Cart Page'
 
+
 @app.route('/get-cart/', methods = ['GET'])
-def projects():
+def get_cart():
     return 'The Get Cart Barcode Page'
