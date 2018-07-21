3ef4ce4917fedb62b2b2752dbb917cccbc05637d
Zach <zcox@alightanalytics.com>
2018-07-21 16:06:02 -0500

    Add routes for scripts.

--- smart_cart/app.py
+++ smart_cart/app.py
@@ -31,3 +31,33 @@ def view_cart():
 @app.route('/get-cart/', methods=['GET'])
 def get_cart():
     return 'The Get Cart Barcode Page'
+
+
+@app.route('/main.js', methods=['GET'])
+def main_js():
+    return send_from_directory('static/SmartCart/', 'main.js')
+
+
+@app.route('/polyfills.js', methods=['GET'])
+def polyfills_js():
+    return send_from_directory('static/SmartCart/', 'polyfills.js')
+
+
+@app.route('/runtime.js', methods=['GET'])
+def runtime_js():
+    return send_from_directory('static/SmartCart/', 'runtime.js')
+
+
+@app.route('/scripts.js', methods=['GET'])
+def scripts_js():
+    return send_from_directory('static/SmartCart/', 'scripts.js')
+
+
+@app.route('/styles.js', methods=['GET'])
+def styles_js():
+    return send_from_directory('static/SmartCart/', 'styles.js')
+
+
+@app.route('/vendor.js', methods=['GET'])
+def vendor_js():
+    return send_from_directory('static/SmartCart/', 'vendor.js')
