da2fdd89c62c0ea170e389a694806f449213cdd7
Jon Falkenstein <jtfalkenstein@gmail.com>
2018-07-21 18:23:04 -0500

    adding debouncer module

--- smart_cart/javascript/src/app/app.module.ts
+++ smart_cart/javascript/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ScannerComponent } from './scanner/scanner.component';
+import { Debouncer } from './debouncer'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { ScannerComponent } from './scanner/scanner.component';
   imports: [
     BrowserModule,
   ],
-  providers: [],
+  providers: [
+    Debouncer
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
