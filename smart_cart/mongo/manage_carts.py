e31952c1b7e623ede3fd9e79c9ea73d57f7b4ebe
Mike Wieners <mwieners@alightanalytics.com>
2018-07-21 17:30:45 -0500

    more cart management

--- smart_cart/mongo/manage_carts.py
+++ smart_cart/mongo/manage_carts.py
@@ -18,10 +18,13 @@ class CartManager(object):
     def close_connection(client):
         client.close()
 
+    def get_carts_collection(self, client):
+        db = client.smart_cart
+        return db.carts
+
     def create_cart(self):
         client = self.open_connection()
-        db = client.smart_cart
-        carts = db.carts
+        carts = self.get_carts_collection(client)
         empty_cart = {'items': []}
         new_cart = carts.insert_one(empty_cart)
         cart_object_id = new_cart.inserted_id
@@ -30,7 +33,7 @@ class CartManager(object):
 
     def add_item_to_cart(self):
         client = self.open_connection()
-        carts = client.smart_cart
+        carts = self.get_carts_collection(client)
         this_cart = carts.find_one(self.cart_id)
         this_cart['items'].append(self.item)
         return cart_contents
@@ -38,8 +41,13 @@ class CartManager(object):
     def remove_item_from_cart(self):
         return cart_contents
 
-    def get_cart(self):
-        return cart_id
+    def get_cart(self, cart_id, client=None):
+        if not client:
+            client = self.open_connection()
+        carts = self.get_carts_collection(client)
+        this_cart = carts.find_one({'_id': cart_id})
+        self.close_connection(client)
+        return this_cart
 
     def get_cart_contents():
         return cart_contents
