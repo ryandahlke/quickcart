18e9881d5977337d424d7bb05f7f342e0c169f7d
Jon Falkenstein <jtfalkenstein@gmail.com>
2018-07-21 18:25:54 -0500

    making scanner and debouncer

---
+++ smart_cart/javascript/src/app/scanner/scanner.component.ts
@@ -0,0 +1,77 @@
+import {Component, OnInit} from '@angular/core';
+import * as Quagga from 'quagga';
+import {Debouncer} from '../debouncer'
+
+@Component({
+  selector: 'app-scanner',
+  templateUrl: './scanner.component.html',
+  styleUrls: ['./scanner.component.css']
+})
+export class ScannerComponent implements OnInit {
+  readonly configObject;
+  private onDetected;
+  private audio;
+
+  constructor(debouncer: Debouncer) {
+    this.configObject = {
+      inputStream: {
+        type: "LiveStream",
+        constraints: {
+          facingMode: "environment", // or user,
+        },
+        target: '#scanner'
+      },
+      locator: {
+        patchSize: "medium",
+        halfSample: false,
+      },
+      frequency: 10,
+      decoder: {
+        readers: [
+          'upc_reader'
+        ],
+        debug: {
+          drawBoundingBox: true,
+          showFrequency: true,
+          drawScanline: true,
+          showPattern: true
+        },
+        multiple: false
+      },
+      locate: true
+    };
+
+    this.onDetected = debouncer.debounce(this.processResult, 1000);
+  }
+
+  ngOnInit() {
+    this.initializeQuagga();
+  }
+
+  initializeQuagga() {
+    let self = this;
+
+    Quagga.init(
+      this.configObject,
+      function (err) {
+        if (err) {
+          console.log(err);
+          return
+        }
+
+        Quagga.onDetected(self.onDetected);
+
+        Quagga.start();
+      }
+    );
+  }
+
+  processResult(result){
+    if (result && result.codeResult) {
+      this.audio = new Audio();
+      this.audio.src = "./assets/beep.mp3";
+      this.audio.load();
+      this.audio.play();
+    }
+  }
+}
