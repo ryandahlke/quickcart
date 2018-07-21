982bb3c431f76d7b93bab2ef5e1c71b8feaeed1e
Zach Allen <zallen@alightanalytics.com>
2018-07-21 18:40:46 -0500

    add checkout-cart and view-cart

---
+++ smart_cart/javascript/src/app/view-cart/view-cart.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-view-cart',
+  templateUrl: './view-cart.component.html',
+  styleUrls: ['./view-cart.component.css'],
+  encapsulation: ViewEncapsulation.Native
+})
+export class ViewCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
