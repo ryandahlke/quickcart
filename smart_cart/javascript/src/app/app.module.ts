6ab544f56c7d1875b12a82579a46143d12a939b5
Zach Allen <zallen@alightanalytics.com>
2018-07-21 16:11:28 -0500

    create base app template

--- smart_cart/javascript/src/app/app.module.ts
+++ smart_cart/javascript/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WindowComponent } from './window/window.component';
+import { MenuComponent } from './menu/menu.component';
+import { ScannerComponent } from './scanner/scanner.component';
+import { CartComponent } from './cart/cart.component';
+import { SelectCartComponent } from './select-cart/select-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WindowComponent,
+    MenuComponent,
+    ScannerComponent,
+    CartComponent,
+    SelectCartComponent
   ],
   imports: [
     BrowserModule,
---
+++ smart_cart/javascript/src/app/select-cart/select-cart.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-select-cart',
+  templateUrl: './select-cart.component.html',
+  styleUrls: ['./select-cart.component.css'],
+  encapsulation: ViewEncapsulation.Native
+})
+export class SelectCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
