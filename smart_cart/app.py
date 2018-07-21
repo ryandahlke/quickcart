0182131352a2bc2aa2b26091e35d53d62ce49f71
Zach <zcox@alightanalytics.com>
2018-07-21 12:42:34 -0500

    Serve index page, fix pep8 standards.

--- smart_cart/app.py
+++ smart_cart/app.py
@@ -1,32 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def index():
-    return 'Index Page'
+    return render_template('static/index.html')
 
 
-@app.route('/start-cart/', methods = ['POST'])
+@app.route('/start-cart/', methods=['POST'])
 def start_cart():
     return 'The Start Cart Page'
 
 
-@app.route('/add-item/', methods = ['POST'])
+@app.route('/add-item/', methods=['POST'])
 def add_item():
     return 'The Add Item Page'
 
 
-@app.route('/delete-item/', methods = ['DELETE'])
+@app.route('/delete-item/', methods=['DELETE'])
 def delete_item():
     return 'The Delete Item Page'
 
 
-@app.route('/view-cart/', methods = ['GET'])
+@app.route('/view-cart/', methods=['GET'])
 def view_cart():
     return 'The View Cart Page'
 
 
-@app.route('/get-cart/', methods = ['GET'])
+@app.route('/get-cart/', methods=['GET'])
 def get_cart():
     return 'The Get Cart Barcode Page'
