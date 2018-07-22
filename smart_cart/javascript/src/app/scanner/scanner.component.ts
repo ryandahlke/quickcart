b864bf2ca6227ee82edf0818e3c482b08b45c9cb
Jon Falkenstein <jtfalkenstein@gmail.com>
2018-07-21 23:34:56 -0500

    debouncing the scanner properly

--- smart_cart/javascript/src/app/scanner/scanner.component.ts
+++ smart_cart/javascript/src/app/scanner/scanner.component.ts
@@ -1,20 +1,26 @@
-import {Component, AfterViewInit, Output, EventEmitter} from '@angular/core';
+import {Component, AfterViewInit, OnDestroy, Output, EventEmitter} from '@angular/core';
 import * as Quagga from 'quagga';
-import {Debouncer} from '../debouncer'
+import {Subject} from "rxjs/index";
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.component.html',
   styleUrls: ['./scanner.component.css']
 })
-export class ScannerComponent implements AfterViewInit {
+export class ScannerComponent implements AfterViewInit, OnDestroy {
   readonly configObject;
-  readonly debouncer;
   readonly audio: HTMLAudioElement;
+  readonly failureThreshhold = .1;
+
+  private lastResult;
 
   @Output() scan: EventEmitter<any> = new EventEmitter();
+  debouncer = new Subject<string>();
 
-  constructor(debouncer: Debouncer) {
+  constructor() {
+    this.audio = new Audio();
+
     this.configObject = {
       inputStream: {
         type: "LiveStream",
@@ -24,10 +30,10 @@ export class ScannerComponent implements AfterViewInit {
         target: '#scanner'
       },
       locator: {
-        patchSize: "medium",
+        patchSize: "large",
         halfSample: false,
       },
-      frequency: 10,
+      frequency: 30,
       decoder: {
         readers: [
           'upc_reader'
@@ -42,14 +48,18 @@ export class ScannerComponent implements AfterViewInit {
       },
       locate: true
     };
-    this.debouncer = debouncer;
 
-    this.audio = new Audio()
+    this.debouncer.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      (value: string) => { this.scan.emit(value)}
+    );
+
+
   }
 
   ngAfterViewInit() {
     this.initializeQuagga();
-
     this.audio.src = "./assets/beep.mp3";
     this.audio.load();
   }
@@ -65,21 +75,48 @@ export class ScannerComponent implements AfterViewInit {
           return
         }
 
-      Quagga.onDetected(
-        self.debouncer.debounce((result)=>{
-          if (result && result.codeResult) {
-            self.beep();
-            self.scan.emit(result);
+        Quagga.onDetected((result) => {
+          if (!result.codeResult) {
+            return;
           }
-        })
-      );
+
+          let code = result.codeResult.code;
+
+          if (self.lastResult !== code) {
+            let countDecodedCodes = 0;
+            let err = 0;
+
+            result.codeResult.decodedCodes.forEach((decodedCode) => {
+              if (decodedCode.error != undefined) {
+                countDecodedCodes++;
+                err += parseFloat(decodedCode.error);
+              }
+            });
+
+            if (err / countDecodedCodes < self.failureThreshhold) {
+              self.beep();
+                  self.debouncer.next(result.codeResult.code);
+
+            } else {
+              console.log(err / countDecodedCodes);
+              console.log("Code: " + code.toString());
+            }
+          }
+
+        });
 
         Quagga.start();
       }
     );
+
+
   }
   beep(){
     this.audio.play();
+  }
+
+  ngOnDestroy(){
+    Quagga.stop();
   }
 
 }
