9aae8d781dff4333d3bb8ea34b986778b33025dc
Jon Falkenstein <jtfalkenstein@gmail.com>
2018-07-21 20:34:09 -0500

    creating route

--- smart_cart/javascript/src/app/app-routing.module.ts
+++ smart_cart/javascript/src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SelectCartComponent} from './select-cart/select-cart.component'
+import { ViewCartComponent} from "./view-cart/view-cart.component";
+
 
 const routes: Routes = [
   {
     path: 'select-cart',
     component: SelectCartComponent
+  },
+  {
+    path: 'view-cart',
+    component: ViewCartComponent
   },
   { path: '',
     redirectTo: 'select-cart',
