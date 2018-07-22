a1163bc8a7582f0a390bdb9a81f1b4b077ae5b08
Jon Falkenstein <jtfalkenstein@gmail.com>
2018-07-21 23:50:44 -0500

    creating cart-item class

---
+++ smart_cart/javascript/src/app/cart-item.ts
@@ -0,0 +1,14 @@
+export class CartItem {
+  upc: string;
+  imageUrl: string;
+  description: string;
+
+  constructor(upc: string){this.upc = upc;}
+
+  // constructor( upc: string, imageUrl: string, description: string){
+  //   this.upc = upc;
+  //   this.imageUrl = imageUrl;
+  //   this.description = description;
+  // };
+
+}
